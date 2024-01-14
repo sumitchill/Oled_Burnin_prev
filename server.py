1129c433f3f3f3cd13c34ed66a915f41710da561
sumitchill <sumitgiri469@gmail.com>
2024-01-14 23:05:26 +0530

    added prog

---
+++ server.py
@@ -0,0 +1,56 @@
+
+from flask import Flask
+import subprocess
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/turnon')
+def turn_on():
+
+    # uses voice assistant to greet!
+    # subprocess.Popen(["nircmd.exe", "speak", "text",'"Hellosir"', "2", "80"], shell=True)
+    
+     # move cursor command used above to move the cursor and bypass the autosleep after monitor turns on!)
+    subprocess.run(["nircmd.exe","beep","500","1000"],shell=True)
+    subprocess.run(["nircmd.exe", "monitor", "on"], shell=True)
+
+    # Uses to move the cursor from a specific Co-ordinates!
+    # subprocess.run(["nircmd", "movecursor", "90", "75"], shell=True)
+    subprocess.run(["nircmd", "movecursor", "96", "75"], shell=True)
+    # subprocess.run(["nircmd", "movecursor", "90", "32"], shell=True)
+    # subprocess.run(["nircmd.exe", "monitor", "on"], shell=True)
+
+
+
+    # Turn on the monitor (But the monitor turnsOff again after some seconds
+    # the request do work but the screen doesn't stay awake so we'll use
+   
+    # subprocess.Popen(["nircmd.exe","screensaver"],shell=True)
+
+    # subprocess.run(["nircmd.exe", "exitwin", "logoff"], shell=True)
+
+    # subprocess.run(["nircmd.exe", "exitwin", "poweroff"], shell=True)
+
+    # time.sleep(700)
+    return "Screen turned on"
+
+
+@app.route('/turnoff')
+def turn_off():
+    # Uses screen off function in nircmd
+    subprocess.run(["nircmd.exe", "monitor", "async_off"], shell=True)
+    # subprocess.run(["nircmd.exe", "standby"], shell=True)
+    return "Screen turned off"
+
+
+@app.route("/screensaver")
+def screensaver():
+    # uses the hibernate command to hibernate the laptop uses 1 hrs time interval!
+    subprocess.Popen(["nircmd.exe", "hibernate"], shell=True)
+    return "screensaver"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
